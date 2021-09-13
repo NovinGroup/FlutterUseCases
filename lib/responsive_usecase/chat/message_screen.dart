893f1fbfeaf9b446ea14fc5dc9d2bcfd791930ff
مهدیار اورعی <mahdiyar.oraei@gmial.com>
2021-09-13 16:35:50 +0600

    Add chat & message screens

---
+++ lib/responsive_usecase/chat/message_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MessageScreen extends StatelessWidget {
+  const MessageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
