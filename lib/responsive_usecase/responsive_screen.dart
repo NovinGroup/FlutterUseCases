472d2443085361a67b1e0dcca10fca76dad844f0
مهدیار اورعی <mahdiyar.oraei@gmial.com>
2021-09-13 16:19:26 +0600

    Create Responsive Screen

---
+++ lib/responsive_usecase/responsive_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ResponsiveScreen extends StatelessWidget {
+  const ResponsiveScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
